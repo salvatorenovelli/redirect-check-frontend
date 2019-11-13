6e21680d82410854e8267108f2932c2848fc969d
Salvatore Novelli <salvatore.novelli@gmail.com>
2019-11-13 21:25:17 +0000

    Releasing 0.3.0 - adding branding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import FileUploader from './FileUploader'
 import Tasks from './Tasks'
+import {Container, Row} from "reactstrap";
 
 
 export default () =>
     <div className="App">
         <div className="App-header">
-            <h2>Welcome to Redirect Check!</h2>
+            <h2>Welcome to My SEO Toolbox</h2>
+            <div className={"logo"}>
+                <img width={"120px"} alt="logo" src={"/img/logo_nobg.png"}/>
+            </div>
+
+            <Container>
+                <Row>
+
+                </Row>
+            </Container>
+
         </div>
         <div className="container">
             <FileUploader/>
